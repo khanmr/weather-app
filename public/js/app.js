4edacdab30b8814f4332ba926d8570d94e611f9c
Rizwan Khan <rizwan.khan@msn.com>
2020-01-11 23:02:46 -0500

    added location button

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
+const locationButton = document.querySelector("#location");
 
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
@@ -23,4 +24,28 @@ weatherForm.addEventListener("submit", e => {
   });
 
   search.value = "";
+});
+
+locationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation not supported.");
+  }
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const location = `${position.coords.longitude},${position.coords.latitude}`;
+
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
+
+    fetch(`/weather?address=${location}`).then(response => {
+      response.json().then(data => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    });
+  });
 });
